a32b6bdda27bdf9f866b1430e8791e084e9a47c1
Myhr86 <stein_myhr@hotmail.com>
2019-04-22 20:53:42 +0200

    Completed given tasks
    Made a contact form in index.html and added valitation in script.js, for the first assignment.
    
    Made two for loops in order to console.log the data from the JSON.
    
    Answered the questions in the .txt file

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,36 @@
 //Use RegEx to validate form
+function validateForm() {
+  var fname = document.forms["contactForm"]["fname"];
+  var lname = document.forms["contactForm"]["lname"];
+  var telephone = document.forms["contactForm"]["telephone"];
+
+  var phone = document.forms["contactForm"]["telephone"].value;
+  var phoneRegEx = /^[0-9][\s./0-9]*$/;
+  var phoneResult = phoneRegEx.test(phone);
+
+  var email = document.forms["contactForm"]["email"].value;
+  var emailRegEx = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9\-\.]+)\.([a-zA-Z]{2,5})$/;
+  var emailResult = emailRegEx.test(email);
+
+
+  if (fname.value == "") {
+    alert("Enter your first name!");
+    return false;
+  }
+
+  if (lname.value == "") {
+    alert("Enter your last name!");
+    return false;
+  }
+
+  if (phoneResult == false) {
+    alert("Enter a valid phone number!");
+    return false;
+  }
+
+  if (emailResult == false) {
+    alert("Enter a valid email!");
+    return false;
+  }
+
+}
--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,34 @@
-//Console.log out elements in JSON file
+var data = {
+	"video": [{
+		"id": 12312412312,
+		"name": "Ecuaciones Diferenciales",
+		"url": "/video/math/edo/12312412312",
+		"author": {
+			"data": [{
+				"name_author": "Alejandro Morales",
+				"url": "/author/alejandro-morales",
+				"type": "master"
+			}]
+		}
+	}]
+}
+
+var output = "";
+
+for (var i = 0; i <= data.video.length; i++) {
+	for (key in data.video[i]) {
+		if (data.video[i].hasOwnProperty(key)) {
+			console.log(output + data.video[i][key]);
+		}
+	}
+}
+
+var output2 = "";
+
+for (var i = 0; i <= data['video'][0]['author'].data.length; i++) {
+	for (key in data['video'][0]['author'].data[i]) {
+		if (data['video'][0]['author'].data[i].hasOwnProperty(key)) {
+			console.log(output2 + data['video'][0]['author'].data[i][key]);
+		}
+	}
+}
